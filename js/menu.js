dfaa84bf46f9f8e1e987997e6d2232feae329797
AnnaChyzhyk <anna.chyzhyk91@gmail.com>
2022-01-05 18:28:23 +0200

    mob-menu

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,33 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector('.header-button__open'),
-    closeMenuBtn: document.querySelector('.header-button__close'),
+    // openMenuBtn: document.querySelector('.header-button__open'),
+    // closeMenuBtn: document.querySelector('.header-button__close'),
+    headerBurger: document.querySelector('.heder__burger'),
+    Burger: document.querySelector('.burger'),
     menu: document.querySelector('.mob-menu'),
+    body: document.querySelector('body')
   };
 
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  // refs.openMenuBtn.addEventListener('click', toggleMenu);
+  // refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.headerBurger.addEventListener('click', toggleMenu);
+  window.addEventListener('resize', resizeCallback);
+
+  function resizeCallback() {
+    const windowWidth = window.innerWidth;
+    console.log(windowWidth + 'px');
+    if (windowWidth >= 768) {    
+      refs.headerBurger.classList.remove('is-hidden');
+      refs.Burger.classList.remove('is-hidden');
+      refs.menu.classList.add('is-hidden');
+      refs.body.classList.remove('lock');
+    }
+  }
 
   function toggleMenu() {
+    refs.headerBurger.classList.toggle('is-hidden');
+    refs.Burger.classList.toggle('is-hidden');
     refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('lock');
   }
 })();
